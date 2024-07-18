7c12cf56c8a89f42991f50abb1820e004afecf0b
Siddhant Khare <siddhantkhare056@gmail.com>
2024-07-18 14:39:35 +0530

    fix save card bug

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,47 +1,47 @@
-import React, { useState,Suspense } from 'react'
-import { useRecipe } from '../context/RecipeContext'
-import {Link} from "react-router-dom"
-import { v4 } from 'uuid'
+import React from "react";
+import { useRecipe } from "../context/RecipeContext";
+import { Link } from "react-router-dom";
 
+const Card = ({ recipe }) => {
+  const { saveRecipe, unsaveRecipe, toggle, savedRecipes } = useRecipe();
 
-const Card = ({recipe}) => {
+  const isPresent = savedRecipes.find((savedRec) => savedRec.id === recipe.id);
 
-  
-  const{saveRecipe,unsaveRecipe,toggle}=useRecipe()
-  
-  
-  
-
-  const toggleSave=()=>{
-    if(!recipe.saved){
-      saveRecipe({...recipe,saved:true})
-      recipe.saved=true
-      
+  const toggleSave = () => {
+    if (!isPresent) {
+      saveRecipe({ ...recipe, saved: true });
+    } else {
+      unsaveRecipe(recipe, recipe.id);
     }
-    else{
-      unsaveRecipe(recipe,recipe.id)
-      recipe.saved=false
-      
-    }
-      toggle(recipe.id)
-  }
+    toggle(recipe.id);
+  };
 
   return (
-    <div className='w-[300px] h-[310px] rounded-3xl overflow-hidden flex flex-col  gap-2'>
-      <Link 
-      to={`/details/${recipe.id}`}
-      className='w-full h-[85%] rounded-3xl object-cover object-center overflow-hidden bg-[#474747b6]'>
-        <img className='rounded-3xl h-full w-full object-cover object-center' src={recipe?.recipe?.image} alt={recipe?.recipe?.label} />
+    <div className="w-[300px] h-[310px] rounded-3xl overflow-hidden flex flex-col  gap-2">
+      <Link
+        to={`/details/${recipe.id}`}
+        className="w-full h-[85%] rounded-3xl object-cover object-center overflow-hidden bg-[#474747b6]"
+      >
+        <img
+          className="object-cover object-center w-full h-full rounded-3xl"
+          src={recipe?.recipe?.image}
+          alt={recipe?.recipe?.label}
+        />
       </Link>
-      <div className=' h-[15%] px-2 mb-2 flex items-start gap-4 justify-between overflow-hidden'>
-      <p className='font-bold text-ellipsis w-[80%] cursor-default'>{recipe?.recipe?.label}</p>
-      <span
-       className='cursor-pointer text-lg'
-       onClick={toggleSave}>
-        {!recipe.saved?<i className="fa-regular fa-bookmark"></i>:<i className="fa-solid fa-bookmark"></i>}</span>
+      <div className=" h-[15%] px-2 mb-2 flex items-start gap-4 justify-between overflow-hidden">
+        <p className="font-bold text-ellipsis w-[80%] cursor-default">
+          {recipe?.recipe?.label}
+        </p>
+        <span className="text-lg cursor-pointer" onClick={toggleSave}>
+          {!isPresent ? (
+            <i className="fa-regular fa-bookmark"></i>
+          ) : (
+            <i className="fa-solid fa-bookmark"></i>
+          )}
+        </span>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Card;
